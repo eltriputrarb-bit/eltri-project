2d4d44507a80baacddd0b78f4e784bd0931f3889
ELTRI <eltriputrarb@gmail.com>
2026-06-19 07:49:50 +0800

    fix: rename JS only sema

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -10,20 +10,15 @@ const cssPath = './build/module/css';
 const jsFile = fs.readdirSync(jsPath).find(f => f.startsWith('main.') && f.endsWith('.js'));
 if (jsFile) {
   fs.renameSync(jsPath + '/' + jsFile, jsPath + '/eltrihideen.js');
-  html = html.replace(new RegExp('/module/js/' + jsFile, 'g'), '/module/js/eltrihideen.js');
+  html = html.replace(new RegExp('/module/js/' + jsFile, 'g'), '/static/js/eltrihideen.js');
 }
 
-// Rename CSS + hapus source map comment
+// Rename CSS
 const cssFile = fs.readdirSync(cssPath).find(f => f.startsWith('main.') && f.endsWith('.css'));
 if (cssFile) {
   fs.renameSync(cssPath + '/' + cssFile, cssPath + '/eltriKatolik.css');
-  html = html.replace(new RegExp('/module/css/' + cssFile, 'g'), '/module/css/eltriKatolik.css');
-  
-  // Hapus source map reference
-  let css = fs.readFileSync(cssPath + '/eltriKatolik.css', 'utf8');
-  css = css.replace(/\/\*#\s*sourceMappingURL=.*?\*\//g, '');
-  fs.writeFileSync(cssPath + '/eltriKatolik.css', css, 'utf8');
+  html = html.replace(new RegExp('/module/css/' + cssFile, 'g'), '/static/css/eltriKatolik.css');
 }
 
 fs.writeFileSync(htmlPath, html, 'utf8');
-console.log('✅ Done! JS & CSS renamed, source map removed!');
+console.log('✅ Done!');
